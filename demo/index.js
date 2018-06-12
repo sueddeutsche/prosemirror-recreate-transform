7d71dea0239ed34d5d756c865b0564582be1737b
Johannes Wilm <mail@johanneswilm.org>
2018-06-12 15:48:09 +0200

    handle merges that don't clash

--- demo/index.js
+++ demo/index.js
@@ -1,12 +1,15 @@
 import {
-    EditorState
+    EditorState,
+    Plugin,
+    PluginKey
 } from "prosemirror-state"
 import {
     EditorView
 } from "prosemirror-view"
 import {
     Schema,
-    DOMParser
+    DOMParser,
+    DOMSerializer
 } from "prosemirror-model"
 import {
     schema
@@ -17,6 +20,10 @@ import {
 import {
     exampleSetup
 } from "prosemirror-example-setup"
+import {
+    Decoration,
+    DecorationSet
+} from "prosemirror-view"
 
 import {
     recreateSteps,
@@ -45,7 +52,43 @@ window.view2 = new EditorView(document.querySelector("#editor2"), {
 document.getElementById('compare').addEventListener('click', () => {
     let tr1 = recreateSteps(state.doc, view1.state.doc)
     let tr2 = recreateSteps(state.doc, view2.state.doc)
-    console.log({tr1, tr2})
-    let tr = mergeTransforms(state.doc, tr1, tr2)
-    console.log({tr})
+    let {tr, changes} = mergeTransforms(state.doc, tr1, tr2)
+    let decos = DecorationSet.empty
+    changes.inserted.forEach(insertion => {
+        decos = decos.add(tr.doc, [
+            Decoration.inline(insertion.from, insertion.to, {class: `insertion user-${insertion.data.user}`}, {})
+        ])
+    })
+    changes.deleted.forEach(deletion => {
+
+        let dom = document.createElement('span')
+        dom.setAttribute('class', `deletion user-${deletion.data.user}`)
+
+        dom.appendChild(
+            DOMSerializer.fromSchema(mySchema).serializeFragment(deletion.slice.content)
+        )
+
+        decos = decos.add(tr.doc, [
+            Decoration.widget(deletion.pos, dom, {})
+        ])
+    })
+
+    let mergedState = EditorState.create({
+        doc: tr.doc,
+        plugins: [
+            new Plugin({
+                key: new PluginKey('diffs'),
+                props: {
+                    decorations(state) {
+                        return decos
+        			}
+                },
+                filterTransaction: tr => false
+            })
+        ]
+    })
+
+    window.view3 = new EditorView(document.querySelector("#editor3"), {
+        state: mergedState
+    })
 })
--- src/merge_transforms.js
+++ src/merge_transforms.js
@@ -1,10 +1,16 @@
 import {
     Transform
 } from "prosemirror-transform"
+import {
+    ChangeSet
+} from "prosemirror-changeset"
 
 export function mergeTransforms(doc, tr1, tr2) {
-    let tr = new Transform(doc)
+    let tr = new Transform(doc),
+        changes = ChangeSet.create(doc)
+
     tr1.steps.forEach(step => tr.step(step))
+    changes = changes.addSteps(tr.doc, tr.mapping.maps, {user: 1})
     tr2.steps.forEach(step => {
         let mapped = step.map(tr1.mapping)
         if (!mapped) {
@@ -13,6 +19,7 @@ export function mergeTransforms(doc, tr1, tr2) {
         }
         tr.step(mapped)
     })
+    changes = changes.addSteps(tr.doc, tr.mapping.maps.slice(tr1.steps.length), {user: 2})
 
-    return tr
+    return {tr, changes}
 }
