ac6dfbe85a6ec42336d68b660c8459effe3b2c0f
Sascha Goldhofer <post@saschagoldhofer.de>
2020-03-28 17:30:17 +0100

    Cleanup webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -33,82 +33,8 @@ const config = {
                         configFile: path.resolve(__dirname, "tsconfig.json")
                     }
                 }
-            },
-            {
-                test: /index\.scss$/,
-                use: [
-                    "file-loader?name=main.css",
-                    "extract-loader",
-                    "css-loader",
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            sourceMaps: true,
-                            includePaths: [
-                                path.join(__dirname, "node_modules")
-                            ]
-                        }
-                    }
-                ]
-            },
-            {
-                test: /index\.html$/,
-                use: [
-                    "file-loader?name=index.html",
-                    "extract-loader",
-                    {
-                        loader: "html-loader",
-                        options: {
-                            interpolate: true,
-                            root: encodeURIComponent(__dirname),
-                            attrs: ["img:src", "source:srcset", "img:data-src", "video:poster"]
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.scss$/,
-                exclude: /index\.scss$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            sourceMaps: true,
-                            includePaths: [
-                                path.join(__dirname, "node_modules")
-                            ]
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    "style-loader",
-                    "css-loader"
-                ]
-            },
-            {
-                test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff\d?$|\.ttf$|\.eot|\.otf|\.wav$|\.mp3$/,
-                use: [
-                    {
-                        loader: "url-loader",
-                        options: {
-                            limit: 1000,
-                            name: "[name]-[sha256:hash:hex:16].[ext]"
-                        }
-                    }
-                ]
             }
         ]
-    },
-
-    devServer: {
-        port: 8071,
-        disableHostCheck: true,
-        host: "0.0.0.0"
     }
 };
 
--- webpack.mocha.js
+++ webpack.mocha.js
@@ -33,74 +33,6 @@ const config = {
                         configFile: path.resolve(ROOT, "./test/tsconfig.json")
                     }
                 }
-            },
-            {
-                test: /index\.scss$/,
-                use: [
-                    "file-loader?name=main.css",
-                    "extract-loader",
-                    "css-loader",
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            sourceMaps: true,
-                            includePaths: [
-                                path.join(ROOT, "node_modules")
-                            ]
-                        }
-                    }
-                ]
-            },
-            {
-                test: /index\.html$/,
-                use: [
-                    "file-loader?name=index.html",
-                    "extract-loader",
-                    {
-                        loader: "html-loader",
-                        options: {
-                            interpolate: true,
-                            root: encodeURIComponent(ROOT),
-                            attrs: ["img:src", "source:srcset", "img:data-src", "video:poster"]
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.scss$/,
-                exclude: /index\.scss$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            sourceMaps: true,
-                            includePaths: [
-                                path.join(ROOT, "node_modules")
-                            ]
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    "style-loader",
-                    "css-loader"
-                ]
-            },
-            {
-                test: /\.jpe?g$|\.gif$|\.png$|\.svg$|\.woff\d?$|\.ttf$|\.eot|\.otf|\.wav$|\.mp3$/,
-                use: [
-                    {
-                        loader: "url-loader",
-                        options: {
-                            limit: 1000,
-                            name: "[name]-[sha256:hash:hex:16].[ext]"
-                        }
-                    }
-                ]
             }
         ]
     },
@@ -115,13 +47,7 @@ const config = {
                 scripts: ["mocha --exit --require source-map-support/register --require ./test/mocha.setup.js testbuild/main.js"]
             }
         })
-    ],
-
-    devServer: {
-        port: 8071,
-        disableHostCheck: true,
-        host: "0.0.0.0"
-    }
+    ]
 };
 
 
